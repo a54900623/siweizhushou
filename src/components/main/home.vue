5adc5b70a93ee3b3e9bba9ac0789515c52e63fc8
a54900623 <1052577343@qq.com>
2018-06-05 19:35:19 +0800

    Initial commit

---
+++ src/components/main/home.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="home"></div>
+</template>
+
+<script>
+  import {Toast} from 'mint-ui'
+
+  export default {
+    name: 'home',
+    data () {
+      return {
+        msg: ''
+      }
+    },
+    created: function () {
+      this.showToast()
+    },
+    methods: {
+      showToast: function () {
+        Toast('提示信息')
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
